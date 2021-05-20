d90af4a8029205a632909cc3d47e8dbceaad3a01
Newell, Riley (Contractor) <Riley_Newell@comcast.com>
2021-05-20 11:50:51 -0500

    Fully implemented Enterprise Desktop

--- scripts/Install-SEP.ps1
+++ scripts/Install-SEP.ps1
@@ -1,7 +1,11 @@
 Start-Transcript  -PATH "C:\Comcast\InstallSEP.txt"
 
-Start-Process  "\\ccwvdPROD.file.core.windows.net\imager\ScriptApps\SymantectEndPointProtection\Symantec Endpoint Protection version 14.2.5569.2100 - English\Setup.exe"
+Expand-Archive -Path "C:\Comcast\InstallSEP-WVD.zip" -DestinationPath "C:\Comcast\" -Force
+
+Start-Process  "C:\Comcast\SEP\Setup.exe"
 
 Start-Sleep -s 60
+
+Start-Process "C:\Comcast\ClientSideClonePrepTool\ClientSideClonePrepTool.exe"
 
 Stop-Transcript
