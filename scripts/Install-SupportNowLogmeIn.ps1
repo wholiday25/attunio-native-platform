56be0b175d0346d439ae92f2bb97b8dc66982e7e
Schlegel, Mark <Mark_Schlegel@comcast.com>
2021-06-15 11:03:33 -0400

    FixedLogMeIN-had relative file path

--- scripts/Install-SupportNowLogmeIn.ps1
+++ scripts/Install-SupportNowLogmeIn.ps1
@@ -3,7 +3,7 @@ Start-Transcript  -PATH "C:\Comcast\Install-SupportNowLogmeIn.txt"
 Expand-Archive -Path "C:\Comcast\Install-SupportNowLogmeIn.zip" -DestinationPath "C:\Comcast\SupportNowLogMeIn" -Force
 
 #Install LogMeIn
-Start-Process .\LogMeInRescueTechnicianConsoleApp.msi -ArgumentList " /qn /norestart"
+Start-Process -FilePath "C:\Comcast\SupportNowLogMeIn\LogMeInRescueTechnicianConsoleApp.msi" -ArgumentList " /qn /norestart"
 Start-Sleep 60
 
 Remove-Item "C:\Comcast\Install-SupportNowLogmeIn.zip" -Force
