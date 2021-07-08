b87a7186f9e73ee879870286fc258365c93bb664
Schlegel, Mark <Mark_Schlegel@comcast.com>
2021-07-08 10:03:08 -0400

    Creative cloud-new timout.,using Start-Process

--- scripts/Install-AdobeCreativeCloud.ps1
+++ scripts/Install-AdobeCreativeCloud.ps1
@@ -7,7 +7,7 @@ Expand-Archive -Path "C:\Comcast\Install-AdobeCreativeCloud.zip" -DestinationPat
 #Install Adobe Creative Cloud 
 
 Start-Process -Filepath "C:\Comcast\AdobeCreativeCloud\Adobe Creative Cloud All Apps\Build\setup.exe" -ArgumentList " --silent" -Wait
-# "C:\Comcast\AdobeCreativeCloud\Adobe Creative Cloud All Apps\Build\setup.exe --silent"
+#"C:\Comcast\AdobeCreativeCloud\Adobe Creative Cloud All Apps\Build\setup.exe --silent"
 
 Remove-Item "C:\Comcast\Install-AdobeCreativeCloud.zip" -Force
 
