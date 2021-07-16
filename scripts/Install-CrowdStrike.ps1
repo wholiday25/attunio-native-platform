b4a89f452a5f93340ff39b946bb15ddbf4bd48ce
Schlegel, Mark <Mark_Schlegel@comcast.com>
2021-07-16 10:43:49 -0400

    Added CrowdStrike to EntDesk as test

---
+++ scripts/Install-CrowdStrike.ps1
@@ -0,0 +1,20 @@
+Start-Transcript  -PATH "C:\Comcast\InstallCrowdStrike.txt"
+
+Expand-Archive -Path "C:\Comcast\InstallCrowdStrike-WVD.zip" -DestinationPath "C:\Comcast\CrowdStrike" -Force
+
+# According to error logs, Power servivce must be running to install Crowdstrike
+# Error 0x80004005: Required service DISABLED: power
+# A required Windows service is disabled, stopped, or missing. Please see the installation log for details.
+
+Set-Service -name Power -Startuptype Automatic
+Start-Service -name Power
+
+# Full list is WinHttpAutoProxySvc,lmhosts,Dhcp,BFE,Dnscache,nsi, power
+
+Start-Process C:\Comcast\Crowdstrike\Crowdstrike-WindowsSensor-6.23.13702.exe -ArgumentList "/silent /install CID=1AB23F2E88784A788D3F7142081CDEFA-8A VDI=1 NO_START=1 GROUPING_TAGS=WVD, Comcast_SEP_Removal"
+
+Start-Sleep -s 60
+
+Remove-Item "C:\Comcast\CrowdStrike\" -Recurse -Force
+
+Stop-Transcript
