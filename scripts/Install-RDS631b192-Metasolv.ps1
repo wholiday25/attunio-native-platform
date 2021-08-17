5357b23f93f542bc9ce86f45830ff00e64e486f9
Schlegel, Mark <Mark_Schlegel@comcast.com>
2021-08-17 13:29:10 -0400

    RDS UNified-"DEV" Metasolv, FireFox (Salesforce)

--- scripts/Install-RDS631b192-Metasolv.ps1
+++ scripts/Install-RDS631b192-Metasolv.ps1
@@ -6,7 +6,10 @@ Expand-Archive -Path "C:\Comcast\Install-RDS631b192-Metasolv.zip" -DestinationPa
 #Create Shortcut on Public Desktop
 New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "MetaSolv" -Value "C:\Program Files\Metasolv\RDS631b192\tbs.exe"
 
-
 Remove-Item "C:\Comcast\Install-RDS631b192-Metasolv.zip" -Force
+
+#UnZip DEV archive, no MSI installer
+Expand-Archive -Path "C:\Comcast\Install-MetasolvDEV.zip" -DestinationPath "C:\Program Files\Metasolv" -Force
+New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "MetaSolvDEV" -Value "C:\Program Files\Metasolv\MetasolvDEV\tbs.exe"
 
 Stop-Transcript
