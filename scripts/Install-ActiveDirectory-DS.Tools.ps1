a2a36fa970652b426d8d30da9035c4cef46130fa
Justin Robbins <jrober566@cable.comcast.com>
2021-08-26 10:17:08 -0400

    Removed Wildcard Depends *

--- scripts/Install-ActiveDirectory-DS.Tools.ps1
+++ scripts/Install-ActiveDirectory-DS.Tools.ps1
@@ -1,3 +1,3 @@
 #Install Active Directory DS LDS Tools
 WRITE-OUTPUT "Installing RSAT Tools for Active Directory"
-Add-WindowsCapability -online -Name "Rsat.ActiveDirectory*"
+Add-WindowsCapability -online -Name "Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0"
