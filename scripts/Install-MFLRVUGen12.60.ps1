8327128dd1178c4d835651d2ab53dac50ec0e9d2
Mark Schlegel <Mark_Schlegel@comcast.com>
2021-10-20 17:15:20 -0400

    Dev- remove VUGEN 12.60 ( non patched)

--- scripts/Install-MFLRVUGen12.60.ps1
+++ scripts/Install-MFLRVUGen12.60.ps1
@@ -2,11 +2,11 @@ Start-Transcript  -PATH "C:\Comcast\Install-MFLRVUGen12.60.txt"
 
 Expand-Archive -Path "C:\Comcast\Install-MFLRVUGen12.60.zip" -DestinationPath "C:\Comcast\Installers" -Force
 
-Start-Process -filepath "C:\Windows\System32\msiexec.exe" -Argumentlist "/i ""C:\Comcast\Installers\Micro Focus Virtual User Generator 12.60\Vugen_x64.msi"" /qn /l*v c:\comcast\VUGen12.60Log.txt /norestart"
+Start-Process -filepath "C:\Windows\System32\msiexec.exe" -Argumentlist "/i ""C:\Comcast\Installers\Micro Focus Virtual User Generator 12.60\Vugen_x64.msi"" /qn"
 Start-Sleep -s 480
 
 Remove-Item "C:\Comcast\Install-MFLRVUGen12.60.zip" -Force
 
-Remove-Item "C:\Comcast\Installers\Micro Focus Virtual User Generator 12.60" -Recurse -Force
+# Remove-Item "C:\Comcast\Installers\Micro Focus Virtual User Generator 12.60" -Recurse -Force
 
 Stop-Transcript
