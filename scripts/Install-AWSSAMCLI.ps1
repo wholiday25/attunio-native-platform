e4e21723b337d04b90abeab0178dab43568aebd7
Mark Schlegel <Mark_Schlegel@comcast.com>
2021-11-18 13:52:01 -0500

    Added AWS SAM CLI to Developer, updated READ.ME

---
+++ scripts/Install-AWSSAMCLI.ps1
@@ -0,0 +1,10 @@
+<#
+This script installs AWS CLI 2.0  via Chocolatey
+#>
+
+# Install Chocolatey if it isn't already installed
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install AWS CLI
+choco install choco install awssamcli -y --no-progress
+
