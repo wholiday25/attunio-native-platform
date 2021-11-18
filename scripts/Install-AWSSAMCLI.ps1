8bc0053711c02d33d289698d1a100a935c2b11df
Mark Schlegel <Mark_Schlegel@comcast.com>
2021-11-18 14:47:47 -0500

    update

--- scripts/Install-AWSSAMCLI.ps1
+++ scripts/Install-AWSSAMCLI.ps1
@@ -6,5 +6,5 @@ This script installs AWS CLI 2.0  via Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install AWS CLI
-choco install choco install awssamcli -y --no-progress
+choco install awssamcli -y --no-progress
 
