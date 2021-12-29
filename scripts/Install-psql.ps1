12fc452ea58a8b86d711bf75d764bebd7aae315f
Mark Schlegel <Mark_Schlegel@comcast.com>
2021-12-29 14:24:51 -0500

    DEV-NSE-added jq, telnet, psql, insomnia

---
+++ scripts/Install-psql.ps1
@@ -0,0 +1,11 @@
+<#
+This script installs psql via Chocolatey
+note that this is only the commandline tools install of POSTgresSQL, since we do not want the DB engine on AVD
+#>
+
+# Install Chocolatey if it isn't already installed
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install psql
+choco install postgresql --installarguments '--enable-components commandlinetools' -y --no-progress 
+
