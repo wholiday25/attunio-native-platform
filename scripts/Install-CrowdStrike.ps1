67d483428f5ddbc8bd6c511f5223e3f09e139ecc
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-01-13 14:45:41 -0500

    Update Crowdstrike version

--- scripts/Install-CrowdStrike.ps1
+++ scripts/Install-CrowdStrike.ps1
@@ -1,6 +1,6 @@
 Start-Transcript  -PATH "C:\Comcast\InstallCrowdStrike.txt"
 
-Expand-Archive -Path "C:\Comcast\InstallCrowdStrike-WVD.zip" -DestinationPath "C:\Comcast\CrowdStrike" -Force
+Expand-Archive -Path "C:\Comcast\InstallCrowdStrike-AVD.zip" -DestinationPath "C:\Comcast\CrowdStrike" -Force
 
 # According to error logs, Power servivce must be running to install Crowdstrike
 # Error 0x80004005: Required service DISABLED: power
@@ -14,7 +14,7 @@ Start-Service -name Power
 # Changed 9/13/2021 to address duplciates in Crowdstrike Console
 # Start-Process -Filepath "C:\Comcast\Crowdstrike\Crowdstrike-WindowsSensor-6.23.13702.exe" -ArgumentList "/silent /install CID=1AB23F2E88784A788D3F7142081CDEFA-8A VDI=1 NO_START=1 GROUPING_TAGS=WVD, Comcast_SEP_Removal"
 
-Start-Process -Filepath "C:\Comcast\Crowdstrike\Crowdstrike-WindowsSensor-6.23.13702.exe" -ArgumentList "/silent /install CID=1AB23F2E88784A788D3F7142081CDEFA-8A VDI=1 GROUPING_TAGS=WVD, AVD, Comcast_SEP_Removal"
+Start-Process -Filepath "C:\Comcast\Crowdstrike\Crowdstrike-WindowsSensor-6.33.14704.exe" -ArgumentList "/silent /install CID=1AB23F2E88784A788D3F7142081CDEFA-8A VDI=1 GROUPING_TAGS=AVD,Comcast_SEP_Removal"
 Set-Itemproperty -path "HKLM:\SYSTEM\CurrentControlSet\Control" -Name 'ServicesPipeTimeout' -Value 600000
 Start-Sleep -s 60
 
