169b708efe9f1084e9fc8c7a2350e6e8b886ec18
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-02-15 15:18:57 -0500

    Software requests 2/15/2022

---
+++ scripts/Install-Go.ps1
@@ -0,0 +1,10 @@
+<#
+This script installs Go programming language via Chocolatey
+#>
+
+# Install Chocolatey if it isn't already installed
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install dbeaver 21.x (Community Edition)
+choco install go -y --no-progress
+
