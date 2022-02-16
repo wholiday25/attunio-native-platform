c638cd6fa192521943bda43abdbfc8ab648bde05
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-02-16 11:00:13 -0500

    added JDK 11 to Dev, NSE

--- scripts/Install-openjdk11microsoft.ps1
+++ scripts/Install-openjdk11microsoft.ps1
@@ -1,12 +1,12 @@
 <#
-This script installs JDK 17.02,  via Chocolatey
-Oracle JDK is a distribution of OpenJDK provided by Oracle
-Latest End Of Life  see https://endoflife.date/java
+This script installs JDK 11.0.14 via Chocolatey
+11.0.14 is Latest End of Life see https://endoflife.date/java
+Microsoft build of OpenJDK
 #>
 
 # Install Chocolatey if it isn't already installed
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install JDK 8
-choco install oracle17jdk -y --no-progress
+choco install microsoft-openjdk11 -y --no-progress
 
