5585341fbcb7fd2578b5b14bb574f31f0f2116e2
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-02-17 14:47:41 -0500

    Replaced JDK 11 Microsoft with OpenJDK

--- scripts/Install-openjdk11.ps1
+++ scripts/Install-openjdk11.ps1
@@ -1,12 +1,11 @@
 <#
-This script installs JDK 11.0.14 via Chocolatey
+This script installs Open JDK 11.0.13.8 via Chocolatey
 11.0.14 is Latest End of Life see https://endoflife.date/java
-Microsoft build of OpenJDK
 #>
 
 # Install Chocolatey if it isn't already installed
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install JDK 8
-choco install microsoft-openjdk11 -y --no-progress
+choco install openjdk11 -y --no-progress
 
