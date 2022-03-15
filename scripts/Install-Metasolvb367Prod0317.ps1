e650c4bf24a8535fa640bfd967865649f6435cfd
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-03-15 11:51:03 -0400

    Update Metasolv Prod to  Metasolvb367Prod0317

---
+++ scripts/Install-Metasolvb367Prod0317.ps1
@@ -0,0 +1,12 @@
+Start-Transcript  -PATH "C:\Comcast\Install-Metasolvb367Prod0317.txt"
+
+#UnZip archive, no MSI installer
+Expand-Archive -Path "C:\Comcast\Install-Metasolvb367Prod0317.zip" -DestinationPath "C:\Program Files\Metasolv" -Force
+
+#Create Shortcut on Public Desktop
+New-Item -ItemType SymbolicLink -Path "C:\Users\Public\Desktop" -Name "MetaSolv" -Value "C:\Program Files\Metasolv\Metasolvb367Prod0317\tbs.exe"
+
+# Don't remove, troubleshooting 8/17/2021
+#Remove-Item "C:\Comcast\Install-MetasolvProd09172021.zip" -Force
+
+Stop-Transcript
