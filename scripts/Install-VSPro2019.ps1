be75f7c10e8ddc034836fffc525160a5caebc81c
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-04-13 14:42:28 -0400

    Added workloads to VSpro2019

--- scripts/Install-VSPro2019.ps1
+++ scripts/Install-VSPro2019.ps1
@@ -3,13 +3,17 @@
 This script installs Visual Studio 2019
 and the .NET 5.0 and Core tools
 ID: Microsoft.VisualStudio.Workload.ManagedDesktop
+4/13/2022 -- Added
+Microsoft.VisualStudio.Workload.NetWeb;includeRecommended;includeOptional
+Microsoft.VisualStudio.Workload.NetCoreTools;includeRecommended;includeOptional
+
 #>
 
 Start-Transcript  -PATH "C:\Comcast\Install-VSPro2019.txt"
 
 $startInfo = New-Object System.Diagnostics.ProcessStartInfo
 $startInfo.FileName = "C:\Comcast\vs_professional__2019.exe"
-$startInfo.Arguments = "--nocache --wait --noUpdateInstaller --add Microsoft.VisualStudio.Workload.ManagedDesktop --quiet --norestart"
+$startInfo.Arguments = "--nocache --wait --noUpdateInstaller --add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb;includeRecommended;includeOptional --add Microsoft.VisualStudio.Workload.NetCoreTools;includeRecommended;includeOptional --quiet --norestart"
 $process = New-Object System.Diagnostics.Process
 $process.StartInfo = $startInfo
 $process.Start()
