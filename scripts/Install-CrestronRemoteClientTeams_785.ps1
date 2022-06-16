132a658fb3649bdaf0510f3583c205afacfeecea
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-06-16 13:57:35 -0400

    Pycharm Pro to Developers, CrestonRemote to Ent Desk

---
+++ scripts/Install-CrestronRemoteClientTeams_785.ps1
@@ -0,0 +1,16 @@
+Start-Transcript  -PATH "C:\Comcast\Install-CrestronRemoteClientTeams_785.txt"
+
+Expand-Archive -Path "C:\Comcast\Install-CrestronRemoteClientTeams_785.zip" -DestinationPath "C:\Comcast\CrestronRemoteClientTeams_785"-Force
+
+
+# Declare settings for install switches
+$param= ' /s /v"/qn"'
+
+#Install-Crestron Remote Client For Teams Troubleshooting
+
+Start-Process -Filepath "C:\Comcast\CrestronRemoteClientTeams_785\crestronremoteclient_785.exe" -ArgumentList $param
+Start-Sleep 120
+
+Remove-Item "C:\Comcast\Install-CrestronRemoteClientTeams_785.zip" -Force
+
+Stop-Transcript
