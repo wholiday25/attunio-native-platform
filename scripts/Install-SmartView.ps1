4ad47a22d0faf8ebd6ef44cacd8aedf058655c5a
Mark Schlegel <Mark_Schlegel@comcast.com>
2022-12-07 17:10:00 -0500

    complete

---
+++ scripts/Install-SmartView.ps1
@@ -0,0 +1,9 @@
+Start-Transcript  -PATH "C:\Comcast\Install-SmartView.txt"
+Expand-Archive -Path "C:\Comcast\Install-SmartView.zip" -DestinationPath "C:\Comcast\Install-SmartView" -Force
+
+Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\Install-SmartView\setup_think-cell_30862.msi"" /qn /norestart"
+#msiexec /i "Adobe Campaign Classic v7  Client.msi" /qn /norestart
+
+Start-Sleep 60 
+
+Stop-Transcript
