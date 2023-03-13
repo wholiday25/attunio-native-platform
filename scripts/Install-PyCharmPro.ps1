5e2d17eb07252496ead5be634f32847a8e294640
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-03-13 17:12:06 -0400

    Pycharm and Pycharm Pro have ignore-checksums, to test

--- scripts/Install-PyCharmPro.ps1
+++ scripts/Install-PyCharmPro.ps1
@@ -6,4 +6,5 @@ This script installs Pycharm Professional Edition via Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # Install Pycharm Community Edition
-choco install pycharm -y --no-progress
+#3-13-2023  --ignore-checksums  addded, both community and Pro Pychamr curently failing.  No notice on Chocolatey.  not Win11 -related, fails on Win10 as well
+choco install pycharm -y --no-progress --ignore-checksums
