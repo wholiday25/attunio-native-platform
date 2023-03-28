1580e43921178e58340ac17b1c95557e6f26e12c
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-03-28 14:41:20 -0400

    Workforce- edited VS2019Extensions

--- scripts/Install-VS2019Extensions.ps1
+++ scripts/Install-VS2019Extensions.ps1
@@ -1,15 +1,21 @@
 Start-Transcript  -PATH "C:\Comcast\Install-VS2019Extensions.txt"
 cls
-
-#Start-Process -FilePath "C:\Comcast\SAPGUI\SAP-GUI-RDS-UNIFIED_20210810_1950.exe" -ArgumentList " /silent"
+Expand-Archive -Path "C:\Comcast\Install-VS2019Extensions.zip" -DestinationPath "C:\Comcast\VS2019Extensions" -Force
 
 Start-Process -FilePath "C:\Comcast\VS2019Extensions\Microsoft.DataTools.IntegrationServices.exe" -ArgumentList " /quiet /norestart"
-Start-Sleep 180
+#Visual Studio >>About >> SQL Server Integration Services - 15.0.2000.180
+Start-Sleep 240
+
 Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\VSIXInstaller.exe" -ArgumentList " /quiet C:\Comcast\VS2019Extensions\Microsoft.DataTools.ReportingServices.vsix"
-Start-Sleep 180
+#Visual Studio >>About >> SQL Server Reporting  Services - 15.0.19528.0
+Start-Sleep 240
+
 Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\VS2019Extensions\MicrosoftSSISOracleConnector-SQL19-x64.msi"" /qn /norestart"
-Start-Sleep 180
+#Uninstall >> SQL Server Integration Serivces Oracle Connector version 15.0.2000.215 - Size = 51.6 MB
+Start-Sleep 240
+
 Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\VS2019Extensions\MicrosoftSSISOracleConnector-SQL19-x86.msi"" /qn /norestart"
+#Uninstall >> SQL Server Integration Serivces Oracle Connector version 15.0.2000.215 - Size = 24.3 MB
 Start-Sleep 240
 
 #Remove-Item "C:\Comcast\Install-VS2019Extensions.zip" -Force
