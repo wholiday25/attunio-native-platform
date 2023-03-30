537e89603d793b02118bce769a56b5753d6445e1
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-03-30 14:36:49 -0400

    VS studio  extensions - increase timeout

--- scripts/Install-VS2019Extensions.ps1
+++ scripts/Install-VS2019Extensions.ps1
@@ -4,11 +4,11 @@ Expand-Archive -Path "C:\Comcast\Install-VS2019Extensions.zip" -DestinationPath 
 
 Start-Process -FilePath "C:\Comcast\VS2019Extensions\Microsoft.DataTools.IntegrationServices.exe" -ArgumentList " /quiet /norestart"
 #Visual Studio >>About >> SQL Server Integration Services - 15.0.2000.180
-Start-Sleep 240
+Start-Sleep 720
 
 Start-Process -FilePath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\VSIXInstaller.exe" -ArgumentList " /quiet C:\Comcast\VS2019Extensions\Microsoft.DataTools.ReportingServices.vsix"
 #Visual Studio >>About >> SQL Server Reporting  Services - 15.0.19528.0
-Start-Sleep 240
+Start-Sleep 720
 
 Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\VS2019Extensions\MicrosoftSSISOracleConnector-SQL19-x64.msi"" /qn /norestart"
 #Uninstall >> SQL Server Integration Serivces Oracle Connector version 15.0.2000.215 - Size = 51.6 MB
