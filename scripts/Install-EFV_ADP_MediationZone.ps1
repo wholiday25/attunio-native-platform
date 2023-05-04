a4ffc8309e840aa904c5b508ddbc879693de877e
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-05-04 14:27:00 -0400

    RDS Mediation Zone

--- scripts/Install-EFV_ADP_MediationZone.ps1
+++ scripts/Install-EFV_ADP_MediationZone.ps1
@@ -1,13 +1,10 @@
 #--this installs EFV_ADPMediation Zone Launcher
 
 Start-Transcript  -PATH "C:\Comcast\Install-MediationzoneLauncher.txt"
-Expand-Archive -Path "C:\Comcast\Install-MediationzoneLauncher.zip" -DestinationPath "C:\Comcast" -Force
-
-md "C:\Program Files\Mediation Launcher"
-copy "C:\Comcast\Mediationzone Launcher\*.*" "C:\Program Files\Mediation Launcher"
+Expand-Archive -Path "C:\Comcast\Install-MediationzoneLauncher.zip" -DestinationPath "C:\Program Files\Mediationzone Launcher" -Force
 
 md "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Mediation Launcher"
-copy "C:\Program Files\Mediation Launcher\Mediationzone launcher.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Mediation Launcher\"
+copy "C:\Program Files\Mediationzone Launcher\Mediationzone launcher.lnk" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Mediationzone Launcher\"
 
 
 Stop-Transcript
