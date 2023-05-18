fb457382fb92d4ffcffb698acb060d75dc19bcc7
Srujana Chintalapudi <srujana_chintalapudi@comcast.com>
2023-05-18 10:54:32 -0400

    ThinkCell latest version

--- scripts/Install-ThinkCell.ps1
+++ scripts/Install-ThinkCell.ps1
@@ -1,7 +1,7 @@
 Start-Transcript  -PATH "C:\Comcast\Install-ThinkCell.txt"
 Expand-Archive -Path "C:\Comcast\Install-ThinkCell.zip" -DestinationPath "C:\Comcast\Install-ThinkCell" -Force
 
-Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\Install-ThinkCell\setup_think-cell_30862.msi"" /qn /norestart"
+Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\Install-ThinkCell\setup_think-cellv11_build33054.msi"" /qn /norestart"
 #msiexec /i "Adobe Campaign Classic v7  Client.msi" /qn /norestart
 
 Start-Sleep 60 
