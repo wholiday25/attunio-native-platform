e21d3021dd348e12ded06815af2da2c3bd602ca7
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-08-03 13:37:34 -0400

    Workforce-Update MFLRAnalysis2022R1

---
+++ scripts/Install-MFLRAnalysis2022R1.ps1
@@ -0,0 +1,10 @@
+Start-Transcript  -PATH "C:\Comcast\Install-MFLRAnalysis12.60.txt"
+Expand-Archive -Path "C:\Comcast\Install-MFLRAnalysis2022R1.zip" -DestinationPath "C:\Comcast\Installers\" -Force
+
+Start-Process -filepath "C:\Windows\System32\msiexec.exe" -Argumentlist "/i ""C:\Comcast\Installers\Micro Focus LoadRunner Analysis 2022 R1\Analysis_x64.msi"" /qn"
+
+Remove-Item "C:\Comcast\Install-MFLRAnalysis2022R1.zip" -Force
+
+# Remove-Item "C:\Comcast\Installers\Micro Focus LoadRunner Analysis 12.60\" -Recurse -Force
+
+Stop-Transcript
