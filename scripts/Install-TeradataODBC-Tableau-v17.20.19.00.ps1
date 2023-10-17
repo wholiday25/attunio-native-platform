b052c55ccb520bfaea08324597d9cb106a6ee654
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-10-17 16:24:53 -0400

    EntDesktop - Added Tableau Teradat drivers

---
+++ scripts/Install-TeradataODBC-Tableau-v17.20.19.00.ps1
@@ -0,0 +1,10 @@
+# Install Teradata drivers for Tableau 
+#Version 17.20.19 
+
+Start-Transcript  -PATH "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00.txt"
+Expand-Archive -Path "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00.zip" -DestinationPath "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00" -Force
+
+Start-Process -FilePath "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00\TeradataODBC\silent_install.bat" -ArgumentList "TDWALLET,ODBC"
+Start-Sleep  -s 120 
+
+
