139739628b141184ba9dae42d6274ee13be7f514
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-10-25 10:01:36 -0400

    Ent Desk-Fix Tableau teradata syntax issue

--- scripts/Install-TeradataODBC-Tableau-v17.20.19.00.ps1
+++ scripts/Install-TeradataODBC-Tableau-v17.20.19.00.ps1
@@ -4,7 +4,8 @@
 Start-Transcript  -PATH "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00.txt"
 Expand-Archive -Path "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00.zip" -DestinationPath "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00" -Force
 
-Start-Process -FilePath "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00\TeradataODBC\silent_install.bat" -ArgumentList "TDWALLET,ODBC"
+# 10/25/2023  need single tick quote to encapsulate double quotes below
+Start-Process -FilePath "C:\Comcast\Install-Tableau-TeradataODBC-v17.20.19.00\TeradataODBC\silent_install.bat" -ArgumentList '"TDWALLET,ODBC"'
 Start-Sleep  -s 120 
 
 
