9b5d8b4871cb94459ecb4a6829dc9fed800a18ea
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-10-31 10:28:00 -0400

    IHA-Edit EDPStation install

--- scripts/Install-EDPSTation.ps1
+++ scripts/Install-EDPSTation.ps1
@@ -1,8 +1,8 @@
 #10/30/2023
 #This script installs EDP Station for the first time, using the provided downloaded area and BAT file
-
+Start-Transcript  -PATH "C:\Comcast\Install-EDPStation.txt"
     
-#download Batch file for EDP Station
+#Download Batch file for EDP Station
 $source = "http://10.168.72.113:7080/SelfServe/GetBatch.cgi?install"
 $destination = "c:\Comcast\Install.bat"
 $client = New-Object System.Net.WebClient
@@ -14,5 +14,7 @@ start-process "cmd.exe" "/c  C:\comcast\install.bat"
 # Wait 240 Seconds for the installation to finish
 Start-Sleep -s 240
 
-#Suppress Windows Firewall notifications - yes, will be deprecated, but ther don't seem to be the same commcands in "netsh advfirewall"
+#Suppress Windows Firewall notifications - yes, will be deprecated at some point, but there doesn't seem to be the same commands in "netsh advfirewall"
 netsh firewall set notifications mode = disable profile = all
+
+Stop-Transcript 
