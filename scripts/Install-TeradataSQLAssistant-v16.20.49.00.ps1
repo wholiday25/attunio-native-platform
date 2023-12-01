5d741e18e05c3ffd9f7ec8a805da922517b5e156
Mark Schlegel <Mark_Schlegel@comcast.com>
2023-12-01 15:16:54 -0500

    EntDesk: TeradataSQl Assistant v16.20.49.00

---
+++ scripts/Install-TeradataSQLAssistant-v16.20.49.00.ps1
@@ -0,0 +1,11 @@
+# Install Teradata SQL Assistant
+#Version 16.20.49.00
+
+Start-Transcript  -PATH "C:\Comcast\Install-TeradataSQL Assistant-v16.20.49.00"
+Expand-Archive -Path "C:\Comcast\Install-TeradataSQL Assistant-v16.20.49.00.zip" -DestinationPath "C:\Comcast\" -Force
+
+# 10/25/2023  need single tick quote to encapsulate double quotes below
+Start-Process -FilePath "C:\Comcast\TeradataSQL Assistant-v16.20.49.00\Files\silent_install.bat" -ArgumentList '"BTEQ,FastExport,SQLA"'
+Start-Sleep  -s 300 
+
+
