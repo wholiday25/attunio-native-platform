18e942f645681cc8363deda3693a1bee2103e9ec
Mark Schlegel <Mark_Schlegel@comcast.com>
2024-02-23 11:26:20 -0500

    Workforce-Fix errors in TOAD, OracleClient

--- scripts/Install-OracleTOADv16.0.ps1
+++ scripts/Install-OracleTOADv16.0.ps1
@@ -4,8 +4,8 @@ Start-Transcript  -PATH "C:\Comcast\Install-OracleTOADv16.0.txt"
 Expand-Archive -Path "C:\Comcast\Install-OracleTOADv16.0.zip" -DestinationPath "C:\Comcast\Install-OracleTOADv16.0" -Force
 
 #Execute-MSI -Action Install -Path "ToadforOracle_16.0.90.1509_x64_En.msi" -Parameters "/quiet /norestart RESTRICTIONS=P"
-Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\OracleTOADv16.0\Files\ToadforOracle_16.0.90.1509_x64_En.msi"" /qn /norestart"
-Start-Sleep  -Seconds 120
+Start-Process -FilePath C:\Windows\System32\msiexec.exe -ArgumentList " /i ""C:\Comcast\Install-OracleTOADv16.0\OracleTOADv16.0\Files\ToadforOracle_16.0.90.1509_x64_En.msi"" /qn /norestart"
+Start-Sleep  -Seconds 60
 
 Remove-Item "C:\Comcast\Install-OracleTOADv16.0.zip" -Force
 Remove-Item "C:\Comcast\Install-OracleTOADv16.0" -Recurse -Force
